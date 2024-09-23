4c383887d4a66fd7f9ec37e5838c554c9b9bc20b
maodisa <kontakt@levi--kuhlmann.de>
2024-09-23 22:02:01 +0200

    fixes

--- app/admin/static/js/own/terminal_socket_connect.js
+++ app/admin/static/js/own/terminal_socket_connect.js
@@ -5,7 +5,7 @@ var socket = io.connect('http://' + document.domain + ':' + location.port);
 function sendCommand() {
     var command = document.getElementById('command-input').value;
     if (command.trim() !== '') { // Check for empty input
-        socket.emit('run_command', { data: command });
+        socket.emit('run_command', {data: command});
         document.getElementById('command-input').value = ''; // Clear the input
     }
 }
@@ -15,4 +15,20 @@ socket.on('command_output', function (msg) {
     var terminal = document.getElementById('terminal-output');
     terminal.innerHTML += '<p>' + msg.data + '</p>';
     terminal.scrollTop = terminal.scrollHeight; // Auto-scroll to the bottom
+});
+
+// Eingabefeld auf Enter-Taste reagieren lassen
+document.getElementById('command-input').addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        sendCommand();
+        event.preventDefault(); // Verhindert das Standardverhalten (z.B. Formularübermittlung)
+    }
+});
+
+// Funktion zum Senden von Ctrl+C
+document.addEventListener('keydown', function (event) {
+    if (event.ctrlKey && event.key === 'c') {
+        socket.emit('run_command', {data: 'Ctrl+C'});
+        event.preventDefault(); // Verhindert das Standardverhalten
+    }
 });
