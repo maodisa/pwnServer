d75860ed7d72f3a34ed95206210b62de0e76fb0e
maodisa <kontakt@levi--kuhlmann.de>
2024-09-24 01:13:23 +0200

    fix auto-run

--- app/admin/python/ducky_script/hid_trigger.py
+++ app/admin/python/ducky_script/hid_trigger.py
@@ -1,5 +1,11 @@
+import os
+import sys
+
 from app.admin.blueprints.badUSB.routes import auto_run_if_enabled
 import time
+
+# Den Pfad zum Hauptverzeichnis deiner App hinzufügen
+sys.path.append(os.path.abspath('/home/kali/pwnServer/app/admin/python/ducky_script/'))
 
 # Kurze Verzögerung einbauen, um sicherzustellen, dass das Gerät vollständig erkannt wird
 time.sleep(2)
