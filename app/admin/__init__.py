0654bb2df7292a14ed997935276d6d354921588d
maodisa <kontakt@levi--kuhlmann.de>
2024-09-23 19:57:06 +0200

    added terminal

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -1,18 +1,23 @@
-from flask import (Flask, redirect, url_for)
+from flask import Flask
+from flask_socketio import SocketIO
 
-import os
-
+# SocketIO-Instanz erstellen
+socketio = SocketIO()
 
 def create_admin_app():
     app = Flask(__name__, template_folder='templates')
 
-    # TODO: Herausfinden was das ist und ÄNDERN!!!
+    # TODO: Herausfinden was das ist und ÄNDERN!!! (Es ist der Secret Key für Sessions und CSRF-Schutz)
     app.secret_key = 'SOME KEY'
 
-
     with app.app_context():
+        # Importiere die Blueprint-Routes
         from app.admin.blueprints.core.routes import core
 
+        # Registriere den Blueprint mit der App
         app.register_blueprint(core, url_prefix='/')
+
+        # Initialisiere Flask-SocketIO mit der App
+        socketio.init_app(app)
 
     return app
